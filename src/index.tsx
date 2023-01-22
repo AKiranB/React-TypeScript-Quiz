108456d970173bc9883240b5f21ed6d2df1fa3b5
Kiran Boyle <anthony.kiran.boyle@weeve.network>
2023-01-22 13:07:43 +0530

    fix issue with casing imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./app.css";
+import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import "@fontsource/raleway/400.css";
