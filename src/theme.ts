a8b42c7b66979d380d9379914859fbc839e9518c
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-07-12 12:44:19 +0200

    remove unused import

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, Theme } from "@chakra-ui/react";
+import { extendTheme  } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 const fonts = {
